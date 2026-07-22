2226deec2e15ba621137c1eb260749c2dd92d140
Ahmad Faisal <cal@xcal.local>
2026-07-22 23:41:27 +0800

    Allow GL and SPV to view tanks index, show, and monitoring (read-only for GL/SPV, full access for admin)

--- routes/web.php
+++ routes/web.php
@@ -127,9 +127,9 @@ Route::middleware('auth')->group(function () {
     Route::post('/reports/{id}/reject', [ReportController::class, 'reject'])->name('reports.reject')->middleware('role:gl,spv');
     
     // Tanks
-    Route::get('/tanks', [TankController::class, 'index'])->name('tanks.index')->middleware('role:admin');
+    Route::get('/tanks', [TankController::class, 'index'])->name('tanks.index')->middleware('role:gl,spv,admin');
     Route::get('/tanks/monitoring', [TankController::class, 'monitoring'])->name('tanks.monitoring')->middleware('role:gl,spv,admin');
-    Route::get('/tanks/{id}', [TankController::class, 'show'])->name('tanks.show')->middleware('role:admin');
+    Route::get('/tanks/{id}', [TankController::class, 'show'])->name('tanks.show')->middleware('role:gl,spv,admin');
     Route::get('/tanks/{id}/edit', [TankController::class, 'edit'])->name('tanks.edit')->middleware('role:admin');
     Route::put('/tanks/{id}', [TankController::class, 'update'])->name('tanks.update')->middleware('role:admin');
     Route::get('/tanks/{id}/calibration', [TankController::class, 'calibration'])->name('tanks.calibration')->middleware('role:admin');
